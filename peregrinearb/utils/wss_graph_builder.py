10ed745c0276799e9ba4342f1f6b6af122dc84e1
Ward Bradt <wardbradt5@gmail.com>
2018-11-13 18:59:40 -0500

    updated wss_graph_builder

--- peregrinearb/utils/wss_graph_builder.py
+++ peregrinearb/utils/wss_graph_builder.py
@@ -1,5 +1,5 @@
 import logging
-import math
+from decimal import Decimal
 
 
 __all__ = [
@@ -13,27 +13,28 @@ adapter = logging.getLogger(__name__)
 
 def wss_add_market(graph, symbol, market_data):
     base, quote = symbol.split('/')
-    graph.add_edge(base, quote, weight=float('Inf'), depth=float('Inf'), market_name=symbol,
-                   fee=market_data['taker_fee'], volume=0, no_fee_rate=-float('Inf'), trade_type='SELL')
-    graph.add_edge(quote, base, weight=float('Inf'), depth=float('Inf'), market_name=symbol,
-                   fee=market_data['taker_fee'], volume=0, no_fee_rate=float('Inf'), trade_type='BUY')
+    graph.add_edge(base, quote, weight=Decimal('Inf'), depth=Decimal('Inf'), market_name=symbol,
+                   fee=market_data['taker_fee'], volume=Decimal('0'), no_fee_rate=-Decimal('Inf'), trade_type='SELL')
+    graph.add_edge(quote, base, weight=Decimal('Inf'), depth=Decimal('Inf'), market_name=symbol,
+                   fee=market_data['taker_fee'], volume=Decimal('0'), no_fee_rate=Decimal('Inf'), trade_type='BUY')
 
 
 def wss_update_graph(graph, symbol, side, price, volume, *args):
+    """Must take args because for L3 order books order id is given"""
     base, quote = symbol.split('/')
     fee_scalar = 1 - graph[base][quote]['fee']
     # if the ask was updated
     if side == 'sell':
         opp_could_exist = price < graph[quote][base]['no_fee_rate']
-        graph[quote][base]['weight'] = -math.log(fee_scalar * 1 / price)
-        graph[quote][base]['depth'] = -math.log(volume * price)
+        graph[quote][base]['weight'] = -(fee_scalar * 1 / price).ln()
+        graph[quote][base]['depth'] = -(volume * price).ln()
         graph[quote][base]['volume'] = volume
         graph[quote][base]['no_fee_rate'] = price
     # if the bid was updated
     else:
         opp_could_exist = price > graph[base][quote]['no_fee_rate']
-        graph[base][quote]['weight'] = -math.log(price * fee_scalar)
-        graph[base][quote]['depth'] = -math.log(volume)
+        graph[base][quote]['weight'] = -(price * fee_scalar).ln()
+        graph[base][quote]['depth'] = -volume.ln()
         graph[base][quote]['volume'] = volume
         graph[base][quote]['no_fee_rate'] = price
 
